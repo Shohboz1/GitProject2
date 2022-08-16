cce95cd05415f0220065efb1f431d8c63482e885
Shohboz Rejabaliyev <rejabaliyevshohbozbek2006@gmail.com>
2022-08-16 21:25:28 +0500

    Added more hello feature

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -2,8 +2,11 @@ package work1;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Work1 started");
-        System.out.println("Created Git Repository");
+        System.out.println("Hello");
+        System.out.println("Developing hello feature");
+        System.out.println("Local Repo is already published to GitHub");
+        System.out.println("Doing more work");
+
 
     }
 }
